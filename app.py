8d203138909e16af155b191991dd329151a2903d
Leo <75710600+shouyierban@users.noreply.github.com>
2024-03-15 16:33:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,27 +21,31 @@ def main(wh_url, chat_id, proxy_url, lua_url):
     
     # 如果环境变量不存在或者文件路径为空，输出错误信息并退出
     if not event_path:
-        print("GITHUB_EVENT_PATH environment variable is not set.")
+        print("GITHUB_EVENT_PATH variable is not set.")
         exit(1)
     
     # 读取 event 文件内容
     with open(event_path, 'r') as event_file:
-        payload = json.load(event_file)
-    print(payload)
+        event_data = json.load(event_file)
     # 检查是否有 script 字段
-    if "script" not in payload:
-        print("No script field in payload.")
-        sys.exit(1)
-
-    keywords = payload["script"]
-    # 根据 keywords 来执行命令
-    if keywords == "miss":
-        miss_main(proxy_url, wh_url, chat_id)
-        
-    elif keywords == "lives":
-        shows(wh_url, chat_id, lua_url)
+    if 'client_payload' in event_data:
+        client_payload = event_data['client_payload']
+        if 'script' in client_payload:
+            keywords = client_payload['script']
+            # 根据 keywords 来执行命令
+            if keywords == "miss":
+                miss_main(proxy_url, wh_url, chat_id)
+                
+            elif keywords == "lives":
+                shows(wh_url, chat_id, lua_url)
+            else:
+                print("Invalid keywords option.")
+        else:
+            print("No 'script' found in client_payload.")
+            sys.exit(1)
     else:
-        print("Invalid keywords option.")
+        print("No 'client_payload' found in event data.")
+        sys.exit(1)
 
 if __name__ == "__main__":
     wh_url = os.environ.get("WH_URL", "")
