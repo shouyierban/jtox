b7da7703946f874f173252a52fdf2f8420bf2678
Leo <75710600+shouyierban@users.noreply.github.com>
2024-03-16 16:23:50 +0800

    updata show

--- show.py
+++ show.py
@@ -1,7 +1,7 @@
 # !/usr/bin/python3
 # -*- encoding: utf-8 -*-
 # @File    :   iptv.py
-# @Time    :   2024/03/02 17:12:22
+# @Time    :   2024/03/15 20:47:24
 # @Author  :   Plate • Lets 
 # @Version :   Pythin 3.10.0
 # @Contact :   Plate_Lets@gmail.com
@@ -9,11 +9,12 @@
 # @Desc    :   None
 
 # ----here put the import lib----
-import requests
+from DrissionPage import SessionPage
 from urllib.parse import quote
+import requests
 import json
-import os
 import datetime
+import os
 
 def convert_to_m3u(tv_list):
     # 构建m3u格式的内容
@@ -106,50 +107,118 @@ def save_data(m3u_content, filename):
     with open(filename, "w", encoding="utf-8") as file:
         file.write(m3u_content)
 
-def shows(wh_url, chat_id, lua_url):
-    with open('list.lua', 'r', encoding='utf-8') as file:
-        lua_script = file.read()
-
-    splash_url = lua_url + f'lua_source={quote(lua_script)}'
-    # 发送请求
-    resp = requests.get(splash_url,timeout=60)
-    result = resp.text
-    st_code = resp.status_code
-    print(st_code)
-    down_data = json.loads(result)
-    # 整理格式
-    tv_list = simple_out(down_data)
-    m3u = convert_to_m3u(tv_list)
-    # 保存
-    save_data(m3u, "./srct/tvshows.m3u8")
-
-    # 发送回复信息
-    now_time = datetime.datetime.now()
-    now_time_str = now_time.strftime("%Y-%m-%d %H:%M:%S")
-    if st_code == 200:
-        callback_message = (
-            '🔊已经成功更新了iptv列表\n'
-            f'🕣更新时间: {now_time_str}\n'
-            )
-    else:
-        callback_message = (
-            '❎iptv列表更新失败\n'
-            f'🕣更新时间: {now_time_str}\n'
-        )
-    params = {
-        "chat_id": chat_id,
-        "parse_mode": "markdown",
-        "text": callback_message
+def shows(de_url, tv_url, lua_url, wh_url, chat_id):
+    base_url = de_url + tv_url
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
     }
-    page = requests.post(url=wh_url, params=params)
-    cb_code = page.status_code
-    if cb_code == 200:
-        print('=====发送成功=====')
+
+    page = SessionPage(timeout=20)
+    page.get(url=base_url, headers=headers)
+    code_get = page.response
+    stt_code = code_get.status_code
+    print('==开始：%s==='%stt_code)
+    ip_list = page.eles('xpath://div[@class="box"][2]/span[2]/a/@href')[0]
+    next_url = base_url + ip_list
+    page.get(url=next_url, headers=headers)
+    check_online = page.eles('xpath://div[contains(@class,"result")][1]/div[3]/div')[0].text
+    if '失效' not in check_online:
+        f_url_page = page.eles('xpath://div[contains(@class,"result")][1]/div[1]/a/@href')[0]
+        f_url = base_url + f_url_page
+        print('====进入lua阶段====')
+        l_script = f'''
+        function main(splash, args)
+            splash:go("{f_url}")
+            assert(splash:wait(15))
+            local iptv_list = {{}}
+            while true do
+                for i = 2, 21 do
+                    local iptv_name_element = splash:select('#content > div:nth-child(' .. i .. ') > div.channel > a > div:nth-child(1)')
+                    local iptv_addr_element = splash:select('#content > div:nth-child(' .. i .. ') > div.m3u8 > table > tbody > tr > td:nth-child(2)')
+
+                    if iptv_name_element and iptv_addr_element then
+                        local iptv_name = iptv_name_element:text()
+                        local iptv_addr = iptv_addr_element:text()
+
+                        if iptv_name and iptv_addr then
+                            local data_info = {{
+                                name = iptv_name,
+                                addr = iptv_addr
+                            }}
+                            table.insert(iptv_list, data_info)
+                        else
+                            print("数据不完整，跳过...")
+                            repeat
+                                break
+                            until true
+                        end
+                    else
+                        print("无法找到元素，跳过...")
+                        repeat
+                            break
+                        until true
+                    end
+                end
+
+                local next_bt = splash:select('#Pagination > a.next')
+                if next_bt then
+                    next_bt: mouse_click()
+                else
+                    break
+                end
+            end
+
+            return {{
+                result = iptv_list
+            }}
+        end
+        '''
+        splash_url = lua_url + f'lua_source={quote(l_script)}'
+        # 发送请求
+        resp = requests.get(splash_url,timeout=30)
+        result = resp.text
+        st_code = resp.status_code
+        print('===lua结束：%s==='%st_code)
+        down_data = json.loads(result)
+        # 整理格式
+        tv_list = simple_out(down_data)
+        m3u = convert_to_m3u(tv_list)
+        # 保存
+        save_data(m3u, "./srct/tvshows.m3u8")
+        print('====运行结束===')
+
+        # 发送回复信息
+        now_time = datetime.datetime.now()
+        now_time_str = now_time.strftime("%Y-%m-%d %H:%M:%S")
+        if st_code == 200:
+            callback_message = (
+                '🔊已经成功更新了iptv列表\n'
+                f'🕣更新时间: {now_time_str}\n'
+                )
+        else:
+            callback_message = (
+                '❎iptv列表更新失败\n'
+                f'🕣更新时间: {now_time_str}\n'
+            )
+        params = {
+            "chat_id": chat_id,
+            "parse_mode": "markdown",
+            "text": callback_message
+        }
+        page = requests.post(url=wh_url, params=params)
+        cb_code = page.status_code
+        if cb_code == 200:
+            print('=====发送成功=====')
+        else:
+            print('=====发送失败(状态码: %s)=====' % cb_code)
     else:
-        print('=====发送失败(状态码: %s)=====' % cb_code)
-    
+        print('IP无效，请稍后重试')
+
 if __name__ == '__main__':
     wh_url = os.environ.get("WH_URL", "")
     chat_id = os.environ.get("CHAT_ID", "")
     lua_url = os.environ.get("LUA_RUL", "")
-    shows(wh_url, chat_id, lua_url)
+    de_url = os.environ.get("DE_RUL", "")
+    tv_url = os.environ.get("TV_RUL", "")
+    shows(de_url, tv_url, lua_url, wh_url, chat_id)
+    
