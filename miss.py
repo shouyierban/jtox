c6fc69dd745b23771a0799a689167a93437a69cc
Leo <75710600+shouyierban@users.noreply.github.com>
2024-03-20 17:45:03 +0800

    fix miss zcode

--- miss.py
+++ miss.py
@@ -137,6 +137,8 @@ def z_list(page):
     url =  'https://18av.mm-cg.com/zh/uncensored_makersr/29/Tokyo%20Hot%20(%E6%9D%B1%E4%BA%AC%E7%86%B1)/{0}.html'.format(page_num)
     page = SessionPage()
     page.get(url)
+    resp = page.response
+    print('zp:' + resp.status_code)
     z_name = page.eles('xpath://div[@class="post video_9s"]/div/h3/a/text()')
     z_url = page.eles('xpath://div[@class="post video_9s"]/div/h3/a/@href')
     z_data = []
@@ -145,11 +147,14 @@ def z_list(page):
         fz_name = z_name[vio_mun]
         pz_url = z_url[vio_mun]
         page.get(pz_url)
+        z_resp = page.response
+        print('zfp:' + z_resp.status_code)
+
         z_code = page.eles("xpath://script/text()")
         z_code_str = ''
         for z_code_text in z_code:
             z_code_str += str(z_code_text)
-        z_code_str = z_code_str[-2500:-300]
+        z_code_str = z_code_str[-4000:]
         # print(z_code)
         pattern = r'id=880_*_(\w+)"'
         matche_code = re.search(pattern, z_code_str)
@@ -161,59 +166,68 @@ def z_list(page):
 
 def get_list(proxy_url, pl_url):
     data_list = []
-    for _ in range(5):
-        tokey_index = random.randint(150, 210)
-        high_index = random.randint(10, 215)
-        base_url = proxy_url
-        tokey_url = base_url + 'https://missav.com/dm16/tokyohot?page={0}'.format(tokey_index)
-        high_url = base_url + 'https://missav.com/search/%E9%AB%98%E5%A6%B9?sort=released_at&page={0}'.format(high_index)
+    try:
+        for _ in range(5):
+            tokey_index = random.randint(150, 210)
+            high_index = random.randint(10, 215)
+            base_url = proxy_url
+            tokey_url = base_url + 'https://missav.com/dm16/tokyohot?page={0}'.format(tokey_index)
+            high_url = base_url + 'https://missav.com/search/%E9%AB%98%E5%A6%B9?sort=released_at&page={0}'.format(high_index)
 
-        url_list = [tokey_url, high_url]
-        for url in url_list:
-            headers = {
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-            }
-            page = SessionPage(timeout=5)
-            page.get(url=url, headers=headers)
+            url_list = [tokey_url, high_url]
+            for url in url_list:
+                headers = {
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+                }
+                page = SessionPage(timeout=5)
+                page.get(url=url, headers=headers)
 
-            for _ in range(2):
-                num_vid = random.randint(1, 11)        
-                name_list = page.eles('xpath://div[contains(@class,"my-2")]/a/text()')[num_vid].strip()
-                name_list = name_list.replace(",", "-")
-                url_list = page.eles('xpath://div[contains(@class,"my-2")]/a/@href')[num_vid].strip()
-                
-                if url_list:
-                    if url == tokey_url:
-                        f_url =  get_addr(base_url, url_list) + ',#genre#=东京'
-                    elif url == high_url:
-                        f_url =  get_addr(base_url, url_list) + ',#genre#=高妹'
-                    data_list.append(f'{name_list}, {f_url}')
-                    # print(name_list + ',' + f_url)
+                for _ in range(2):
+                    num_vid = random.randint(1, 11)        
+                    name_list = page.eles('xpath://div[contains(@class,"my-2")]/a/text()')[num_vid].strip()
+                    name_list = name_list.replace(",", "-")
+                    url_list = page.eles('xpath://div[contains(@class,"my-2")]/a/@href')[num_vid].strip()
+                    
+                    if url_list:
+                        if url == tokey_url:
+                            f_url =  get_addr(base_url, url_list) + ',#genre#=东京'
+                        elif url == high_url:
+                            f_url =  get_addr(base_url, url_list) + ',#genre#=高妹'
+                        data_list.append(f'{name_list}, {f_url}')
+                        # print(name_list + ',' + f_url)
+    except Exception as e:
+        print('miss错误' + e)
 
     # 添加pig列表========
-    pig_json = pig_list(pl_url)
-    if 'data' in pig_json:
-        full_data = pig_json['data']
-        # 解析JSON文件，获取name和url字段
-        for key, item in full_data.items():
-            if isinstance(item, dict):
-                pig_name = item.get('name')
-                pig_url = item.get('purl')
-                full_pig_url = proxy_url + pig_url
-                f_pig_url = pig_m3u(full_pig_url)
-                f_pig_url = f_pig_url + ',#genre#=PIG-国产'
-                data_list.append(f'{pig_name}, {f_pig_url}')
-                # print(pig_name + ',' + f_pig_url + '\n')
-    # 添加18tv
-    for _ in range(4):
-        z_num = random.randint(1, 75)
-        z_data = z_list(z_num)
-        for z_adrr in z_data:
-            z_name = z_adrr[0]
-            z_url = z_adrr[1]
-            z_url = z_url + ',#genre#=Z-东京'
-            data_list.append(f'{z_name}, {z_url}')
-    
+    try:
+        pig_json = pig_list(pl_url)
+        if 'data' in pig_json:
+            full_data = pig_json['data']
+            # 解析JSON文件，获取name和url字段
+            for key, item in full_data.items():
+                if isinstance(item, dict):
+                    pig_name = item.get('name')
+                    pig_url = item.get('purl')
+                    full_pig_url = proxy_url + pig_url
+                    f_pig_url = pig_m3u(full_pig_url)
+                    f_pig_url = f_pig_url + ',#genre#=PIG-国产'
+                    data_list.append(f'{pig_name}, {f_pig_url}')
+                    # print(pig_name + ',' + f_pig_url + '\n')
+    except Exception as e:
+        print('pig错误' + e)
+
+    # 添加18tv========
+    try:
+        for _ in range(4):
+            z_num = random.randint(1, 75)
+            z_data = z_list(z_num)
+            for z_adrr in z_data:
+                z_name = z_adrr[0]
+                z_url = z_adrr[1]
+                z_url = z_url + ',#genre#=Z-东京'
+                data_list.append(f'{z_name}, {z_url}')
+    except Exception as e:
+        print('z错误' + e)
     return data_list
 
 def save_data(m3u_content, filename):
